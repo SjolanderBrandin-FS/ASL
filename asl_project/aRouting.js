1a14e9d2f466a9c4b463b02956fd4d2c1bcd8381
Lyss Sjolander <lysssjolander@Lysss-MacBook-Pro.local>
2023-08-07 03:18:53 -0700

    Adv-Routing Assignment Complete

--- asl_project/aRouting.js
+++ asl_project/aRouting.js
@@ -1,8 +1,25 @@
 const express=require('express');
 const app=express();
 
-app.get('/', (request, response)=>{
-	response.send('Home page, GET....');
+// GET /products/all HTTP/1.1
+app.get('/products/all/:pageNum-:sort-:order', (request, response) => {
+	response.send(
+		'GET Products: ' + request.params.pageNum + ', ' + request.params.sort + ', ' + request.params.order
+	);
+});
+
+// GET /products/8179 HTTP/1.1
+app.get('/products/:8719', (request, response) => {
+	response.send(
+		'GET Products: ' + request.params[8719]
+	);
+});
+
+// GET /products/8719-small-red HTTP/1.1
+app.get('/products/:8719-:size-:color', (request, response) => {
+	response.send(
+		'GET Products: ' + request.params[8719] + ', ' + request.params.size + ', ' + request.params.color
+	);
 });
 
 
