4ddc4090dcfb3cf7a465ea7eedf2cba7d70fa345
Lyss Sjolander <lysssjolander@Lysss-MacBook-Pro.local>
2023-08-07 15:52:02 -0700

    restful activity setup complete

---
+++ asl_project/restful.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false}));
+const productRouter = require('../routes/products');
+
+app.use("/products", productRouter);
+
+app.listen(3000);
