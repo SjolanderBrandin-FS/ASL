d8da6507e1a5ef6d0665856801a41e976981ac00
Brandin Sjolander <BTSjolander@student.fullsail.edu>
2023-08-15 14:58:26 -0700

    Create New Shoe issue resolved

--- controllers/products.js
+++ controllers/products.js
@@ -2,25 +2,39 @@ const Products = require('../models/products')
 
 const index = (req, res) => {
     const products = Products.all()
-    res.json(products);
+    // res.json(products);
+    res.render('views/products/index', { products });
 }
+
 const form = (req, res) => {
-    res.send('products.form');
+    //res.send('products.form');
+    if (req.params.id) {
+        const product = Products.locate(req.params.id);
+        res.render('views/products/edit', { product })
+    } else {
+        res.render('views/products/create')
+    }
 }
+
 const show = (req, res) => {
     const product = Products.locate(req.params.id)
-    res.json(product);
+    //res.json(product);
+    res.render('views/products/show', { product });
 }
+
 const create = (req, res) => {
     const product = Products.create(req.body)
-    res.json(product);
+    res.redirect('/products/' + product.id)
 }
+
 const update = (req, res) => {
     const product = Products.update(req.params.id, req.body)
-    res.json(product)
+    res.redirect('/products/' + req.params.id)
 }
+
 const remove = (req, res) => {
     const products = Products.remove(req.params.id)
     res.json(products)
 }
+
 module.exports = { index, form, show, create, update, remove }
--- models/products.js
+++ models/products.js
@@ -11,8 +11,9 @@ const locate = (id) => {
 }
 
 const create = (product) => {
-    products.push(product)
-    return products
+    const id = Number(products[products.length - 1].id) + 1 + ""
+    products.push({ id, ...product })
+    return products[product.length - 1]
 }
 
 const update = (id, product) => {
