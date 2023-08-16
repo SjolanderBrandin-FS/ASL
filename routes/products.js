a1947f86bcf9e7eac7004e1ecfc1a67128da5296
jworkman <jworkmandevelopment@gmail.com>
2023-08-16 07:28:19 -0400

    Fixing routing for product delete
    Adding router.all() instead of router.post since our anchor tag uses the GET method by default.

--- routes/products.js
+++ routes/products.js
@@ -9,5 +9,5 @@ router.get('/:id/edit', productCtrl.form);
 router.post('/', productCtrl.create);
 router.post('/:id', productCtrl.update);
 router.delete('/:id', productCtrl.remove);
-router.post('/:id/delete', productCtrl.remove);
+router.all('/:id/delete', productCtrl.remove);
 module.exports = router
